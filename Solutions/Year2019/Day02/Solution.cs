66c741fee29000899b04d72917816a113ccc8361
Jake Neumann <jneumann@cottinghambutler.com>
2019-12-02 14:49:37 -0600

    AOC 2019 Day 2

--- Solutions/Year2019/Day02/Solution.cs
+++ Solutions/Year2019/Day02/Solution.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
 namespace AdventOfCode.Solutions.Year2019 {
 
     class Day02 : ASolution {
@@ -7,11 +12,85 @@ namespace AdventOfCode.Solutions.Year2019 {
         }
 
         protected override string SolvePartOne() {
-            return null; 
+            int instr = 0;
+            int[] opcode = Input.Split(',').Select(s => int.Parse(s)).ToArray();
+            opcode[1] = 12;
+            opcode[2] = 2;
+
+            while (opcode[instr] != 99)
+            {
+                switch (opcode[instr])
+                {
+                    case 1:
+                        // Add
+                        opcode[opcode[instr + 3]] = opcode[opcode[instr + 1]] + opcode[opcode[instr + 2]];
+                        break;
+                    case 2:
+                        // Multiply
+                        opcode[opcode[instr + 3]] = opcode[opcode[instr + 1]] * opcode[opcode[instr + 2]];
+                        break;
+                }
+
+                instr += 4;
+            }
+
+            System.Console.WriteLine(opcode[0]);
+
+            return null;
         }
 
         protected override string SolvePartTwo() {
-            return null; 
+            int noun = 0;
+            int verb = 0;
+            int instr = 0;
+            bool answerFound = false;
+
+            for (int x = 0; x <= 99; x++)
+            {
+                for (int y = 0; y <= 99; y++)
+                {
+                    int[] opcode = Input.Split(',').Select(s => int.Parse(s)).ToArray();
+
+                    instr = 0;
+                    opcode[1] = x;
+                    opcode[2] = y;
+
+                    while (opcode[instr] != 99)
+                    {
+                        switch (opcode[instr])
+                        {
+                            case 1:
+                                // Add
+                                opcode[opcode[instr + 3]] = opcode[opcode[instr + 1]] + opcode[opcode[instr + 2]];
+                                break;
+                            case 2:
+                                // Multiply
+                                opcode[opcode[instr + 3]] = opcode[opcode[instr + 1]] * opcode[opcode[instr + 2]];
+                                break;
+                        }
+
+                        instr += 4;
+                    }
+
+
+                    if (opcode[0] == 19690720)
+                    {
+                        answerFound = true;
+                        verb = y;
+                        break;
+                    }
+                }
+
+                if (answerFound)
+                {
+                    noun = x;
+                    break;
+                }
+            }
+
+            System.Console.WriteLine(100 * noun + verb);
+
+            return null;
         }
     }
 }
